cbdeee2a973d81b34bfa233a6186a78f8017c361
Ebee02 <beelawrence8@gmail.com>
2025-01-02 13:16:53 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <section className="text-slate-800 antialiased min-h-screen bg-[#FAFAF8]">
+      <Header />
+      <Form />
+    </section>
+  );
+}
+
+export default App;
